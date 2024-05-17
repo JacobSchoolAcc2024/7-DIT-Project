dfd10ecbea7bd5e47ddac35030bb5b4e12dd9dad
JacobSchoolAcc2024 <21btorcino@wakatipu.school.nz>
2024-05-17 22:19:10 +1200

    Added one upgrade for push up and automatic update of the page.
    In the html I added upgrades that would only show when str is enough to buy it.

--- idle_gym.js
+++ idle_gym.js
@@ -1,16 +1,45 @@
-<<<<<<< Updated upstream
+let str = 0;
+let str_gain = 0;
+let multiplier = 1
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "10%";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-=======
-
-let str = 0
-
-function gain_str(increase_by, id){
-    str += increase_by
-    document.getElementById(id).innerHTML = str
->>>>>>> Stashed changes
 }
+
+function gain_str(increase_by, id) {
+    str += increase_by + str_gain
+    document.getElementById(id).innerHTML = "Strenght: " + str;
+    update_window(id, "Strenght :", str);
+}
+
+function upgrade_str(increase_by, cost,id) {
+    if (str >= cost) {
+        multiplier += 0.5
+        cost *= multiplier
+        str -= cost;
+        str_gain += increase_by;
+        document.getElementById("strenght").innerHTML = "Strenght: " + str;
+        update_window(id, "Cost: ", cost * multiplier);
+
+    } else {
+        alert("You need: " + cost + " more strength points");
+    }
+}
+
+function update_window(id, name, variable) {
+    document.getElementById(id).innerHTML = name + variable;
+}
+
+function check_upgrades(){
+    if (str >= cost){
+        document.getElementById("upgrade1_").innerHTML.style = "display: block";
+
+}}
+
+
+setInterval(update_window, 500);
+setInterval(check_upgrades, 500);

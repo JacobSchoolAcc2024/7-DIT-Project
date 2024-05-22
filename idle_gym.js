dfdce995279ab0ea4dcb206094b03796d7be379c
JacobSchoolAcc2024 <21btorcino@wakatipu.school.nz>
2024-05-22 13:21:23 +1200

    Updated Code

--- idle_gym.js
+++ idle_gym.js
@@ -1,6 +1,13 @@
-let str = 0;
-let str_gain = 0;
+
+///Variabels///
+
+let str = 1;
+let str_gain = 1;
 let multiplier = 1;
+let auto_pushup_multiplier = 1
+
+
+/// Html Related JS ///
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "10%";
@@ -29,34 +36,77 @@ function showMainContent() {
 
 
 
-
-
-
-
 /////////////////////////////////////////////////////////////////////////////////////
 /// Strenght and Upgrades section.
-function gain_str(increase_by, id) {
+
+
+function gain_str(increase_by, str_id) {
     str += increase_by + str_gain;
-    document.getElementById(id).innerHTML = "Strenght: " + str;
+    document.getElementById(str_id).innerHTML 
+    = "Strenght: " + formatNumber(str);
 }
+
 
 function upgrade_str(increase_by, cost) {
     upgradeCost = Math.round(cost ** multiplier);
     str -= upgradeCost;
-    str_gain += increase_by;
+    str_gain += (increase_by ** multiplier)*multiplier;
     document.getElementById("strenght").innerHTML = "Strenght: " + str;
+    document.getElementById("strenght_gain").innerHTML = "Strenght Gain: " + str_gain;
     multiplier += 1;    
-    alert("Strenght gain is increased by " + str_gain)
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+/// Automatic gain of strenght.
+
+
+function purchaseAuto(increase_by, id, time, cost) {
+    requiredCost = Math.round(cost ** auto_pushup_multiplier);
+    str -= requiredCost;
+    auto_pushup_multiplier += 1;
+    setInterval(auto_str_gain(increase_by, id), time);
+    console.log("This is the time: " + time)
+    time -= 500;
+
+
+}
+
+function auto_str_gain(increase_by, id){
+    return function(){
+        str += (increase_by * auto_pushup_multiplier) * str_gain;
+        console.log((increase_by * auto_pushup_multiplier) * str_gain)
+    }
 }
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+/// Check for requirements.
+
+
 function checkUpgrades() {
     const upgrades = {
-        upgrade1: {
-            cost_id: "cost1",
-            id: "upgrade1_",
+        Progressive_Overload: {
+            cost_id: "progressive_overload_cost",
+            id: "progressive_overload",
             cost: 10
         },
         // upgrade2: {
@@ -79,21 +129,63 @@ function check_upgrades(id, cost, cost_id) {
         document.getElementById(cost_id).innerHTML = "Cost :" + formatNumber(requiredCost);
     } else {
         document.getElementById(id).style.display = "none";
-        console.log(formatNumber(requiredCost));
     }
 }
 
+function auto_price_pushup(){
+    
+    const auto_prices = {
+        auto_price1: {
+            cost_id: "auto_cost_pushup",
+            id: "auto_pushup",
+            cost: 500,
+        },
+        // auto_price2: {
+        //     cost_id: "auto_price2",
+        //     id: "auto_price2_",
+        //     cost: 1000
+        // },
+    };
+
+    for (const price in auto_prices) {
+        check_auto_price(auto_prices[price].id, auto_prices[price].cost, 
+            auto_prices[price].cost_id);
+    }
+
+}
+
+function check_auto_price(id, cost, cost_id){
+    const requiredCost = Math.round(cost ** auto_pushup_multiplier);
+    if (str >= requiredCost) {
+        document.getElementById(id).style.display = "block";
+        document.getElementById(cost_id).innerHTML = "Cost :" + formatNumber(requiredCost);
+    } else {
+        document.getElementById(id).style.display = "none";
+        console.log("This is the price for auto_pushup: " + formatNumber(requiredCost))
+}
+
+}
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+/// Utilities.
+
+
 function formatNumber(num) {
-    const suffixes = ["", " K", " Million", " Billion", " Trillion"];
+    const suffixes = ["", " K", " Million", " Billion", " Trillion", " Quadrillion"
+        , " Quintillion", " Sextillion", " Septillion", " Octillion", " Nonillion"
+    ];
     const suffixIndex = Math.floor(Math.log10(Math.abs(num)) / 3);
     const formattedNum = parseFloat((num / Math.pow(1000, suffixIndex)).toFixed(2));
     return formattedNum + (suffixes[suffixIndex] || "");
 }
 
-function update_window(){
-    document.getElementById("strenght").innerHTML = "Strenght: " + formatNumber(str);
+function update_window_str() {
+    document.getElementById('strenght').innerHTML = "Strenght: " + formatNumber(str);
 }
 
-
 setInterval(checkUpgrades, 100);
-setInterval(update_window, 100);
+setInterval(update_window_str, 100);
+setInterval(auto_price_pushup, 100);
